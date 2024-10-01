77597ba46a3ee7b585d1a3d6f44de679d9bdec2b
Pedro Henrique Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2024-09-30 22:24:25 -0300

    Update classes.js

--- assets/classes.js
+++ assets/classes.js
@@ -71,29 +71,56 @@ class Stage {
 
     //TODO: Evento do botão de atacar.
 
-    this.fighter1El.querySelector('.attackbutton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2) )
+    this.fighter1El
+      .querySelector(".attackbutton")
+      .addEventListener("click", () =>
+        this.doAttack(this.fighter1, this.fighter2)
+      );
 
-    this.fighter2El.querySelector('.attackbutton').addEventListener("click", () => this.doAttack(this.fighter2, this.fighter1))
+    this.fighter2El
+      .querySelector(".attackbutton")
+      .addEventListener("click", () =>
+        this.doAttack(this.fighter2, this.fighter1)
+      );
   }
 
   update() {
     // Fighter 1
     this.fighter1El.querySelector(
       ".name"
-    ).innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+    ).innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(2)} HP`;
     let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
     this.fighter1El.querySelector(".bar").style.width = `${f1Pct}%`;
 
     // Fighter 2
-    this.fighter2El.querySelector(".name").innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+    this.fighter2El.querySelector(
+      ".name"
+    ).innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
 
     let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
     this.fighter2El.querySelector(".bar").style.width = `${f2Pct}%`;
   }
 
-  doAttack(attacking, attacked){
-    console.log(`${attacking.name} está atacando ${attacked.name}`)
+  doAttack(attacking, attacked) {
+    if (attacking.life <= 0 || attacked.life <= 0) {
+      console.log(`Atacado cachorro morto`);
+      return;
+    }
 
-    this.update()
+    let attackFactor = (Math.random() * 2).toFixed(2);
+    let defenseFactor = (Math.random() * 2).toFixed(2);
+
+    let actualAttack = attacking.attack * attackFactor;
+    let actualdefense = attacked.defense * defenseFactor;
+
+    if (actualAttack > actualdefense) {
+      attacked.life -= actualAttack;
+      console.log(
+        `${attacking.name} causou ${actualAttack.toFixed(2)} dano em ${attacked.name}`
+      );
+    } else {
+      console.log(`${attacked.name} conseguiu defender`);
+    }
+    this.update();
   }
 }
